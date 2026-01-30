b83003dfaa3b53fad29363233ff22d6825c8a554
shivayBadmoss <shivayagarwal08@gmail.com>
2026-01-31 03:06:11 +0530

    S

--- server/index.cjs
+++ server/index.cjs
@@ -248,10 +248,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+  console.log('FINAL LISTEN PORT =>', PORT);
 });
 
 module.exports = app;
